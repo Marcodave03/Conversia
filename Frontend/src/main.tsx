914934cc1492d1b98701c359b18e47be7f039201
Marco Dave <marcodave03@gmail.com>
2025-04-25 09:13:13 +0700

    adding wallet

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -6,25 +6,32 @@ import LandingPage from '../src/pages/LandingPage';
 import './App.css';
 import { AuthProvider } from '../src/context/AuthProvider';
 import ProtectedRoute from './components/ProtectedRoute';
+import { WalletProvider } from "@suiet/wallet-kit";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <AuthProvider>
+    <WalletProvider>
       <Router>
         <Routes>
           <Route path="/landing" element={<LandingPage />} />
+          {/* <Route path="/" element={<App interview_prompt="You are my girlfriend" />}/> */}
           <Route 
             path="/" 
             element={
               <ProtectedRoute>
-                <App interview_prompt="You are my girlfriend" />
+                
+                  <App interview_prompt="You are my girlfriend" />
+                
               </ProtectedRoute>
+              
             } 
           />
         </Routes>
       </Router>
+      </WalletProvider>
     </AuthProvider>
   </React.StrictMode>
 );
--- Frontend/src/pages/LandingPage.tsx
+++ Frontend/src/pages/LandingPage.tsx
@@ -3,8 +3,11 @@ import { useAuth } from '../hooks/useAuth';
 import { useNavigate } from 'react-router-dom';
 import bgImage from '../assets/landing-bg.jpg';
 import logo from '../assets/conversia-lg.png';
+import { addressEllipsis, ConnectButton, useWallet } from "@suiet/wallet-kit";
+import "@suiet/wallet-kit/style.css"; 
 
 const LandingPage: React.FC = () => {
+  const wallet = useWallet();
   const { login, signup } = useAuth();
   const navigate = useNavigate();
   
@@ -41,6 +44,30 @@ const LandingPage: React.FC = () => {
         alt="Conversia Logo"
         className="w-82 h-32 mb-6" 
       />
+
+
+      <ConnectButton />
+      <p>
+        <span className='gradient'>Wallet Address : </span>{wallet.status}
+      </p>
+      {wallet.status === 'connected' && (
+        <>
+          <p>
+            <span className="bg-gradient-to-tr"> Connected Account : </span>
+            {wallet.account?.address}
+          </p>
+          <p>
+            <span className="bg-gradient-to-t">Connected Account with ellipsis</span>
+            {addressEllipsis(wallet.account?.address || '')}
+          </p>
+          <p>
+            <span className="bg-gradient-to-t">Current chain of wallet</span>
+            {wallet.chain?.name}
+          </p>
+          
+          
+        </>
+      )}
 
       <div className="bg-gray-800 bg-opacity-90 p-8 rounded-lg shadow-md w-96">
         <h2 className="text-2xl font-bold mb-4">{isLogin ? 'Sign In' : 'Sign Up'}</h2>
