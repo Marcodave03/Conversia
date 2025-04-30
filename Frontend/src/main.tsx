2091093e522a66c4864987bf02605cbe505ec69f
Marco Dave <marcodave03@gmail.com>
2025-04-30 09:50:06 +0700

    Wallet provider

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './App';
-import LandingPage from '../src/pages/LandingPage';
-import './App.css';
-import { AuthProvider } from '../src/context/AuthProvider';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App";
+import LandingPage from "../src/pages/LandingPage";
+import "./App.css";
+import { AuthProvider } from "../src/context/AuthProvider";
+import { WalletProvider } from "@suiet/wallet-kit";
 // import ProtectedRoute from './components/ProtectedRoute';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <Router>
-        <Routes>
-        <Route path="/" element={
-                <App interview_prompt="You are my girlfriend" />
-            }  />
-        <Route path="/landing" element={<LandingPage />} />
+      <WalletProvider>
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={<App interview_prompt="You are my girlfriend" />}
+            />
+            <Route path="/landing" element={<LandingPage />} />
 
-          {/* <Route path="/landing" element={<LandingPage />} />
+            {/* <Route path="/landing" element={<LandingPage />} />
           <Route 
             path="/" 
             element={
@@ -28,8 +33,9 @@ root.render(
               </ProtectedRoute>
             } 
           /> */}
-        </Routes>
-      </Router>
+          </Routes>
+        </Router>
+      </WalletProvider>
     </AuthProvider>
   </React.StrictMode>
 );
